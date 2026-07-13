99c4474e2f749a9f6a53f9201586212c5756a4a1
Srushtipatil33 <srushtipatil2094@gmail.com>
2026-07-13 10:53:52 +0530

    Add files via upload

---
+++ s4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"enter radious of the circle";
+cin>>a;
+cout<<"circumference of the circle is:"<<2*3.14*a<<endl;
+cout<<"area of the circle is:"<<3.14*a*a<<endl;
+}
