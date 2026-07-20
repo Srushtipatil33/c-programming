44ee10a5ea45fcfc40554301b208c813a7c3c776
Srushtipatil33 <srushtipatil2094@gmail.com>
2026-07-20 10:50:32 +0530

    Add files via upload

---
+++ leap.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"enter any year";
+cin>>a;
+if(a%4==0)
+{
+cout<<" The year is leap";
+}
+else
+{
+cout<<" the year is not leap";
+}
+}
