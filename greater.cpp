4b488ee3747411d3ae52b56ab29124f9dd8f0026
Srushtipatil33 <srushtipatil2094@gmail.com>
2026-07-20 10:41:55 +0530

    Add files via upload

---
+++ greater.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+cout<<"enter any two numbers";
+cin>>num1>>num2;
+if(num1>num2)
+{
+cout<<num1<<" is greater than"<<num2;
+}
+else{
+cout<<num2<<" is greater than"<<num1;}
+}
---
+++ swappingtwonumbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Enter any two number=";
+cin>>a>>b;
+a=a+b;
+b=a-b;
+a=a-b;
+cout<<"after swapping:";
+cout<<"first number=";
+cout<<"second number=";
+}
